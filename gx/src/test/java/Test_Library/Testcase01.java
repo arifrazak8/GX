1b833ef9e79fcce52787410d20bdc4297910520f
arifrazak8 <arif.raza@weavers-web.com>
2024-05-15 13:18:00 +0530

    Add_terminal

--- gx/src/test/java/Test_Library/Testcase01.java
+++ gx/src/test/java/Test_Library/Testcase01.java
@@ -1,6 +1,8 @@
 package Test_Library;
 
 import Generic_Library.Launch;
+import Pom_Library.LINE_HAUL.Terminals_Spots_POM;
+
 import org.testng.annotations.Test;
 
 public class Testcase01 extends Launch {
@@ -15,6 +17,16 @@ public class Testcase01 extends Launch {
 
     @Test (priority = 1)
     void tc02() throws Exception {
-
+ Terminals_Spots_POM p = new Terminals_Spots_POM(driver);
+        p.clickLinehaulBtn();
+        p.clickTerminalsSpotsBtn();
+        p.clickAddNewTerminalBtn();
+        p.setTerminalNumber("8989");
+        p.setTerminalName("Automation");
+        p.setTerminalAbbreviation("123");
+        p.setEmail_txt("test@test.com");
+        p.setPhone_txt("4567894561");
+        p.setAddress_txt("adventz infinity");
+        p.clickAddTerminal_btn();
     }
 }
